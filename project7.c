3b1e7eb6ab97048e578c74a4a51be137a09aae4d
Yury <123@gmail.com>
2017-01-30 03:35:47 -0500

    Added window creation

--- project7.c
+++ project7.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-int main (int argc, char* argv)
+int main (int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO):=0)
+    //SDL initialization
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         printf("Error SDL Init");
         return 1;
     }    
     
-    printf ("Success init!");
+    //Create window
+    SDL_Window* window1 = SDL_CreateWindow 
+    (
+        "Hello!",
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED,
+        640, 
+        480, 
+        0
+    );
+    
+    if(!window1)
+    {
+        printf("Window error!");
+        SDL_Quit();
+        return 1;
+    }
+    
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window1);
+     
+    printf ("Success!\n");
     SDL_Quit();
 }
