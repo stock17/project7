6abc3b274b64b69fa86eebd0ee856932238320dd
Yury <123@gmail.com>
2017-01-30 07:34:18 -0500

    Added background

--- project7.c
+++ project7.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL_image.h>
 
 int main (int argc, char* argv[])
 {
     //SDL initialization
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        printf("Error SDL Init");
+        printf("Error SDL Init: %s", SDL_GetError());
         return 1;
     }    
     
@@ -23,14 +24,61 @@ int main (int argc, char* argv[])
     
     if(!window1)
     {
-        printf("Window error!");
+        printf("Window error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Creating renderer    
+    SDL_Renderer* renderer1 = SDL_CreateRenderer(window1, -1, 
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (!renderer1)
+    {
+        printf("Renderer error: %s", SDL_GetError());
+        SDL_DestroyWindow(window1);
         SDL_Quit();
         return 1;
     }
     
-    SDL_Delay(3000);
+    // Load image in memory
+    SDL_Surface* surface1 = IMG_Load("resources/cloud015.jpg");
+    if (!surface1)
+    {
+        printf("Surface error: %s", SDL_GetError());
+        SDL_DestroyRenderer(renderer1);
+        SDL_DestroyWindow(window1);
+        SDL_Quit();
+        return 1;
+    }
+    
+    // Load image to texture
+    SDL_Texture* texture1 = SDL_CreateTextureFromSurface(renderer1, surface1);
+    SDL_FreeSurface(surface1);
+    if (!texture1)
+    {
+        printf("Texture error: %s", SDL_GetError());
+        SDL_DestroyRenderer(renderer1);
+        SDL_DestroyWindow(window1);
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Clear window
+    SDL_RenderClear(renderer1);
+    
+    // Draw the image
+    SDL_RenderCopy(renderer1, texture1, NULL, NULL);
+    SDL_RenderPresent(renderer1);
+    
+       
+    SDL_Delay(5000);
+
+    SDL_DestroyTexture(texture1);
+    SDL_DestroyRenderer(renderer1);
     SDL_DestroyWindow(window1);
-     
-    printf ("Success!\n");
     SDL_Quit();
+    
+    printf("Success!");
+    return 0;
 }
